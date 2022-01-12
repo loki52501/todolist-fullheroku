ee7044c4078f2ae73b5c5737f0613c02b716f78b
lkslokesh <lkslokesh25@gmail.com>
2022-01-12 19:09:02 +0530

    updated port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const _=require("lodash");
-const port = 8000;
+const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const app = express();
 app.set("view engine", "ejs");
